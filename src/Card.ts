d04f33c67c4f6c3d18f02dbf2ffe2060fbd031b7
DESKTOP-I16DB8E\adrid <adrian.diaz@xarxatecactiva.com>
2026-09-02 18:11:28 +0200

    feat: nueva clase Card creada

---
+++ src/Card.ts
@@ -0,0 +1,25 @@
+export type Suit = "oros" | "copas" | "bastos" | "espadas"
+
+ export class Card {
+    suit: Suit
+    value:number
+
+    constructor(suit: Suit, value: number){
+        this.suit = suit 
+        this.value = value
+    }
+
+    getFigureValue=(): number => {
+        if (this.value == 10) return 8;
+        if (this.value == 11) return 9;
+        if (this.value == 12) return 10;
+        return this.value;
+    }
+
+    getCardId = (): string => {
+        return(`${this.value}-${this.suit}`)
+    }
+}
+const carta = new Card("copas", 12)
+
+console.log(carta.getFigureValue())
