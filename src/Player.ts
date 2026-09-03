2f33d86c0752ea93c9107e78d7bbc680edced0a2
DESKTOP-I16DB8E\adrid <adrian.diaz@xarxatecactiva.com>
2026-09-03 22:29:21 +0200

    feat: nueva clase Player creada

---
+++ src/Player.ts
@@ -0,0 +1,45 @@
+import { Card } from "./Card.js";
+
+export class Player{
+    name: string
+    #hand: Card[] =[];
+    #wonCards: Card[] = []
+    #score: number = 0
+    #scopas: number = 0
+
+    constructor(name:string){
+       this.name = name  
+    }
+
+    getHand(): Card[] {
+        return this.#hand
+    }
+
+    playCard(cardId:string): Card | undefined{
+        const index = this.#hand.findIndex((card) => card.getCardId() === cardId);
+
+        if (index === -1){
+            return undefined
+        }
+        return this.#hand.splice(index, 1)[0]
+    }
+
+    receiveCards(cards: Card[]): void {
+        cards.forEach((card) =>{
+            this.#hand.push(card);
+        });
+    }
+
+    winCards(cards: Card[], isScopa: boolean): void {
+        cards.forEach((card) =>{
+            this.#wonCards.push(card);
+        });
+        if (isScopa) {
+         this.#scopas ++;
+        }
+    }
+
+    hasCardsInHand(): boolean {
+        return this.#hand.length > 0;
+    }
+}
